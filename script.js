c66ccaf1fa708bb047e749f2fecf976b7089a131
Angélica Albuquerque <angelica.o.albuquerque@gmail.com>
2021-02-21 17:59:03 -0300

    mockup transactions

--- script.js
+++ script.js
@@ -15,7 +15,28 @@ const Modal = {
   }
 }
 
+const transactions = [
+  {
+    id: 1,
+    description: 'Luz',
+    amount: -50000,
+    date: '21/02/2021'
+  }, 
+  {    
+    id: 2,
+    description: 'Website',
+    amount: 500000,
+    date: '21/02/2021'}, 
+  {    
+    id: 3,
+    description: 'Internet',
+    amount: -20000,
+    date: '21/02/2021'
+  }
+]
+
 // Eu preciso somar as entradas; depois preciso somar as saídas e remover das entradas o valor das saídas. Assim, terei o total.
+
 const Transaction = {
   incomes() {
     //somar as entradas
